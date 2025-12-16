a9573859458b188e6ec6bce4cf0a38eb4aa80723
A-Tsunashima <e1b23054@oit.ac.jp>
2025-12-16 14:30:44 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z2762/kaizi/janken_x/JankenXApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z2762.kaizi.janken_x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JankenXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JankenXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z2762/kaizi/janken_x/JankenXApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z2762.kaizi.janken_x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankenXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
